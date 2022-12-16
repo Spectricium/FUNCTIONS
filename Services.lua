94ba7e91fe3240b463b41a291b7cfafc6f5c31b1
Spectricium <113564281+Spectricium@users.noreply.github.com>
2022-12-15 20:42:47 -0600

    Update and rename .gitignore to Services.lua

---
+++ Services.lua
@@ -0,0 +1,9 @@
+local Services = {}
+Services.Load = function()
+local GM = game
+local Rep = GM:GetService("ReplicatedStorage")
+local Work = GM:GetService("Workspace")
+local RS = GM:GetService("RunService")
+local Players = GM:GetService("Players")
+end
+return Services
